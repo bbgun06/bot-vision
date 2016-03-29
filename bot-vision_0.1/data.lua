4ac926d798e54625d4765d2e6f9862a736ac41bc
bbgun06 <bbgun06@verizon.net>
2016-03-29 12:51:47 -0400

    bot-vision
    v .1

---
+++ bot-vision_0.1/data.lua
@@ -0,0 +1,6 @@
+--data.raw["logistic-robot"]["logistic-robot"].max_distance_of_nearby_sector_revealed = 1
+--data.raw["construction-robot"]["construction-robot"].max_distance_of_nearby_sector_revealed = 1
+data.raw["locomotive"]["diesel-locomotive"].energy_per_sector = "10MJ",
+    max_distance_of_sector_revealed = "14",
+    max_distance_of_nearby_sector_revealed = "3",
+    energy_per_nearby_scan = "250kJ"
